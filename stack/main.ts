48ae5c05b51b0b5644287651400ef7c46d21663c
joaquimchianca <joaquimchianca@gmail.com>
2023-08-08 14:43:12 -0300

    stack impl

---
+++ stack/main.ts
@@ -0,0 +1,42 @@
+type Node<T> = {
+    value: T;
+    prev?: Node<T>;
+}
+
+export default class Stack<T> {
+    public length: number;
+    private head?: Node<T>
+    
+
+    constructor() {
+        this.head = undefined;
+        this.length = 0;
+    }
+
+    push(item: T): void {
+        const node: Node<T> = {value: item};
+        this.length++;
+        if (!this.head) {
+            this.head = node;
+            return;
+        }
+        node.prev = this.head;
+        this.head = node;
+
+}
+    pop(): T | undefined {
+        if (!this.head) {
+            return undefined;
+        }
+
+        this.length--;
+        let current = this.head;
+        this.head = current.prev;
+        current.prev = undefined;
+        return current.value
+
+}
+    peek(): T | undefined {
+        return this.head?.value;
+}
+}
